8255503b8b40dd0a0f6342fa02519c5e574a745d
tushar-qapita <tushar.devidas@qapita.com>
2024-06-01 16:17:27 +0530

    added experience in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Experience from "./components/Experience";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Navbar />
           <Hero />
           <About />
+          <Experience />
         </div>
       </div>
     </BrowserRouter>
