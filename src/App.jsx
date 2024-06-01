95ede35aa445303dcb1deeef1d03e54682b85f7b
tushar-qapita <tushar.devidas@qapita.com>
2024-06-01 14:25:16 +0530

    added navbar and assets  in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div>Portfolio</div>;
-}
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
